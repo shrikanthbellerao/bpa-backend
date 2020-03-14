f5be675e4233a2beac1ee9bbe397b408e86b4cf8
Shrikanth Rao <shrikrao@cisco.com>
2020-03-14 12:46:29 +0530

    Implemented API call to fetch login JWT & service orders from BPA

--- app.js
+++ app.js
@@ -1,28 +1,121 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const express     = require('express');
+const app         = express();
+const router      = express.Router();
+const mongoose    = require('mongoose');
+const bodyParser  = require('body-parser');
+const compression = require('compression');
+const request     = require('request').defaults({ rejectUnauthorized: false });
 
 const GradesSchema = require('./sample_training.model').GradesSchema;
 
+app.use(bodyParser.json({limit: '10mb'}));    // limit : 10mb is required for File upload
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true }));
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+app.use(compression());
+app.use('/', router);
 
-app.get('/', (req, res) => {
-  res.send('Hi There!');
+router.get('/', (req, res) => {
+  res.send({
+    msg: 'Hi There!'
+  });
+});
+
+var postRequestOptions = {
+  url     : '',
+  method  : 'POST',
+  json    : true,
+  headers : {
+    Accept : 'application/json',
+    'Content-Type' : 'application/json'
+  },
+  body    : {},
+};
+
+var getRequestOptions = {
+  url     : '',
+  method  : 'GET',
+  json    : true,
+  headers : {
+    Accept : 'application/json',
+    'Content-Type' : 'application/json'
+  }
+};
+
+var responseObj = {
+  status  : '',
+  msg     : '',
+  body    : null
+};
+
+// Validate User's credentials to access BPA
+router.post('/login', (req, res) => {
+
+  console.log('POST /login: ', req.body);
+
+  postRequestOptions.url = `https://${req.body.vmIPAddress}/bpa/api/v1.0/login`;
+  postRequestOptions.headers.Authorization = `Basic ${req.body.base64Credential}`;
+
+  request(postRequestOptions, function(error, response, body) {
+
+    console.log('\nResponse Error: ', error);
+    console.log('\nResponse Body: ', body);
+
+    if (error) {
+      responseObj.status  = 'error';
+      responseObj.msg     = `Error Occurred while validating User's credentials. Error Message: ${error}`;
+    } else {
+      responseObj.status  = 'success';
+      responseObj.msg     = 'Successfully validated user credentials';
+      responseObj.body    = body;
+    }
+
+    res.send(responseObj);
+  });
+});
+
+// Fetch Service Orders from Service Catalog microservice of BPA
+router.post('/service-orders', (req, res) => {
+
+  console.log('POST /service-orders: ', req.body);
+
+  getRequestOptions.url = `https://${req.body.vmIPAddress}/bpa/api/v1.0/service-catalog/service-orders`;
+  getRequestOptions.headers.Authorization = `Bearer ${req.body.accessToken}`;
+
+  request(getRequestOptions, function(error, response, body) {
+
+    console.log('\nResponse Error: ', error);
+    console.log('\nResponse Body: ', body);
+
+    if (error) {
+      responseObj.status  = 'error';
+      responseObj.msg     = `Error Occurred while fetching Service Orders. Error Message: ${error}`;
+    } else {
+      responseObj.status  = 'success';
+      responseObj.msg     = 'Successfully fetched Service Orders';
+      responseObj.body    = body;
+    }
+
+    res.send(responseObj);
+  });
 });
 
 app.listen(8080, () => {
 
+  console.log('\n\n');
+  console.log('***********************');
   console.log('Listening on port 8080!');
+  console.log('***********************');
 
-  const dbUrl = "mongodb+srv://bpa:bpa@bpa-mzccx.mongodb.net/sample_training";
-  const connObj = mongoose.createConnection(dbUrl);
-  const GradesModel = connObj.model('Grade', GradesSchema);
+  // const dbUrl = "mongodb+srv://bpa:bpa@bpa-mzccx.mongodb.net/sample_training";
+  // const connObj = mongoose.createConnection(dbUrl);
+  // const GradesModel = connObj.model('Grade', GradesSchema);
 
-  GradesModel.find({}, { }, { limit:2 }, (err, docs) => {
-    console.log('Err: ', err);
-    console.log('Docs: ', docs);
-  });
+  // GradesModel.find({}, { }, { limit:2 }, (err, docs) => {
+  //   console.log('Err: ', err);
+  //   console.log('Docs: ', docs);
+  // });
 });
