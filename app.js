8717d67538635b162ea72d61115065075a259371
aaymahaj <a.mahajan1999@gmail.com>
2020-03-19 12:55:53 +0530

    commit_new

--- app.js
+++ app.js
@@ -107,32 +107,91 @@ router.post('/service-orders', (req, res) => {
   });
 });
 
+// Fetch Milestone of Active Services from Service Catalog microservice of BPA
+router.post('/service-orders', (req, res) => {
 
+  //   console.log('POST /service-orders: ', req.body);
 
-router.get('/milestones', (req, res) => {
+  //   getRequestOptions.url = `https://${req.body.vmIPAddress}/bpa/api/v1.0/service-catalog/service-orders`;
+  //   getRequestOptions.headers.Authorization = `Bearer ${req.body.accessToken}`;
 
-  console.log('POST /service-orders: ', req.body);
+  //   request(getRequestOptions, function (error, response, body) {
 
-  getRequestOptions.url = `https://${req.body.vmIPAddress}/bpa/api/v1.0/milestones/?objectType=service-catalog-order&objectReference=${req.body.id}`;
-  getRequestOptions.headers.Authorization = `Bearer ${req.body.accessToken}`;
+  //     console.log('\nResponse Error: ', error);
+  //     console.log('\nResponse Body: ', body);
 
-  request(getRequestOptions, function (error, response, body) {
+  //     if (error) {
+  //       responseObj.status = 'error';
+  //       responseObj.msg = `Error Occurred while fetching Service Orders. Error Message: ${error}`;
+  //     } else {
+  responseObj.status = 'success';
+  responseObj.msg = 'Successfully fetched Service Orders';
+  responseObj.body = {
+    "status": "Success",
+    "message": "Milestones List",
+    "totalRecords": 10,
+    "data": [
+      {
+        "_id": "5e72223b34ac5c0166164910",
+        "updatedAt": "2020-03-18T13:29:31.773Z",
+        "createdAt": "2020-03-18T13:29:31.773Z",
+        "objectType": "service-catalog-order",
+        "objectReference": "5e72223ae3c240015092efbb",
+        "milestone": "Check-Sync I",
+        "__v": 0,
+        "status": "Complete"
+      },
+      {
+        "_id": "5e72223c34ac5c0166164911",
+        "updatedAt": "2020-03-18T13:29:32.152Z",
+        "createdAt": "2020-03-18T13:29:32.152Z",
+        "objectType": "service-catalog-order",
+        "objectReference": "5e72223ae3c240015092efbb",
+        "milestone": "Dryrun Review I",
+        "__v": 0,
+        "execution": {
+          "type": "dryrun",
+          "executionData": "5e722349104a5741c775e16a",
+          "templateId": "Dry-Run"
+        },
+        "status": "Complete"
+      },
+      {
+        "_id": "5e72223c34ac5c0166164912",
+        "updatedAt": "2020-03-18T13:29:32.287Z",
+        "createdAt": "2020-03-18T13:29:32.287Z",
+        "objectType": "service-catalog-order",
+        "objectReference": "5e72223ae3c240015092efbb",
+        "milestone": "Peer review",
+        "__v": 0,
+        "execution": {
+          "type": "peer-review",
+          "executionData": "5e72223ae3c240015092efbb",
+          "templateId": "Peer Review"
+        },
+        "status": "Complete"
+      },
+      {
+        "_id": "5e72223c34ac5c0166164913",
+        "updatedAt": "2020-03-18T13:29:32.397Z",
+        "createdAt": "2020-03-18T13:29:32.397Z",
+        "objectType": "service-catalog-order",
+        "objectReference": "5e72223ae3c240015092efbb",
+        "milestone": "Pre-change Validation",
+        "__v": 0,
+        "execution": {
+          "type": "template-execution",
+          "executionData": "[{\"deviceName\":\"USPALTWRR01DRE0001-PV01\",\"executionId\":\"5e7229f60d2df741cc8e510d\",\"overallTmplResult\":false}]",
+          "templateId": "DC-MSC-Port-Turn-Down-Pre-Check-Validation"
+        },
+        "status": "Complete"
+      }]
+  };
+  // }
 
-    console.log('\nResponse Error: ', error);
-    console.log('\nResponse Body: ', body);
-
-    if (error) {
-      responseObj.status = 'error';
-      responseObj.msg = `Error Occurred while fetching Service Orders. Error Message: ${error}`;
-    } else {
-      responseObj.status = 'success';
-      responseObj.msg = 'Successfully fetched Service Orders';
-      responseObj.body = body;
-    }
-
-    res.send(responseObj);
-  });
+  res.send(responseObj);
 });
+// });
 
 
 app.listen(8080, () => {
