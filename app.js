30a9caa500b0e88858a188abe2530483e306e8af
lakshmiPriya99 <lp.velupula@gmail.com>
2020-03-30 09:09:52 +0530

    updated app.js

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ router.post('/login', (req, res) => {
         console.log('\nResponse Error: ', error);
         console.log('\nResponse Body: ', body);
 
-        if (body.errCode === 403) {
+        if (error) {
             responseObj.status = 'error';
             responseObj.msg = `Error Occurred while validating User's credentials. Error Message: ${error}`;
         } else {
